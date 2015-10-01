6e9ca6d3d727a415a7af13e22d1d8a91f4802a97
Diego Ragazzi <d@ragazzid.com.br>
2015-10-01 17:03:22 -0300

    Programa finalizado + gitignore 	new file:   .gitignore 	new file:   src/br/com/lanuniversity/fluxocaixa/Start.java

---
+++ src/br/com/lanuniversity/fluxocaixa/Start.java
@@ -0,0 +1,46 @@
+package br.com.lanuniversity.fluxocaixa;
+
+import java.util.Scanner;
+
+public class Start {
+
+    static int quantidadeVenda = 0;
+    static int quantidadeProdutos = 0;
+    static float valorParcial = (float) 0;
+    static float valorTotal = (float) 0;
+
+    public static void main(String[] args) {
+        System.out.println("Digite a quantidade de Vendas:");
+        quantidadeVenda = pegaQuantidadeVenda();
+
+        // Itineramos a quantidade de venda
+        for (int i = 0; i < quantidadeVenda; i++) {
+            quantidadeProdutos = pegaQuantidadeProdutos();
+
+            // Itineramos a quantidade de produtos
+            for (int j = 0; j < quantidadeProdutos; j++) {
+                valorParcial += pegaValorParical();
+            }
+
+            valorTotal += valorParcial;
+            System.out.println("Venda " + (i + 1) + " - Total: R$ " + valorParcial + ".");
+            valorParcial = 0;
+        }
+        
+        System.out.println("* Saldo do dia: R$ "+ valorTotal + ".");
+
+    }
+
+    protected static int pegaQuantidadeVenda() {
+        return (int) new Scanner(System.in).nextInt();
+    }
+
+    protected static int pegaQuantidadeProdutos() {
+        return (int) new Scanner(System.in).nextInt();
+    }
+
+    protected static float pegaValorParical() {
+        return (float) new Scanner(System.in).nextFloat();
+    }
+
+}
